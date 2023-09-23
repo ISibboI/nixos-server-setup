f4590105e13f47f71da879a465b2e3af25a37081
Sebastian Schmidt <isibboi@gmail.com>
2023-09-23 11:25:36 +0300

    Fix syncthing config dir.

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,7 @@
         enable = true;
         user = "syncthing";
         dataDir = "/home/syncthing";    # Default folder for new synced folders
-        configDir = "/home/.config/syncthing";   # Folder for Syncthing's settings and keys
+        configDir = "/home/syncthing/.config/syncthing";   # Folder for Syncthing's settings and keys
     };
   };
 
