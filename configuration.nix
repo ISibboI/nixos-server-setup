30e18474152a99d78a737166b7a22c49b84510ed
Sebastian Schmidt <isibboi@gmail.com>
2023-10-05 14:55:03 +0300

    Syncthing skip host check.

--- configuration.nix
+++ configuration.nix
@@ -168,6 +168,7 @@
         user = "syncthing";
         dataDir = "/home/syncthing";    # Default folder for new synced folders
         configDir = "/home/syncthing/.config/syncthing";   # Folder for Syncthing's settings and keys
+        extraOptions.gui.insecureSkipHostcheck = true;
     };
   };
 
