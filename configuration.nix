668c46b9e36b812d1835fb843079d61cf3d11afb
Sebastian Schmidt <isibboi@gmail.com>
2024-10-23 16:27:04 +0300

    Add forgejo actions runner.

--- configuration.nix
+++ configuration.nix
@@ -372,7 +372,29 @@ in {
         ROOT_URL = "https://forgejo.${config.networking.domain}/";
         PROTOCOL = "http+unix";
       };
+      actions = {
+        ENABLED = true;
+        DEFAULT_ACTIONS_URL = "github";
+      };
       service.DISABLE_REGISTRATION = true; 
+    };
+  };
+
+  # Forgejo actions runner
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-actions-runner;
+    instances.default = {
+      enable = true;
+      name = "monolith";
+      url = "https://forgejo.${config.networking.domain}/";
+      # Obtaining the path to the runner token file may differ
+      tokenFile = config.age.secrets.forgejo-runner-token.path;
+      labels = [
+        "ubuntu-latest:docker://node:16-bullseye"
+        "ubuntu-22.04:docker://node:16-bullseye"
+        "ubuntu-20.04:docker://node:16-bullseye"
+        "ubuntu-18.04:docker://node:16-buster"
+      ];
     };
   };
 
