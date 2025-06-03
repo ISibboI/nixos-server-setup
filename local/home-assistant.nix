a5964953b87417a4801dba38724d0980b53bb4f7
Sebastian Schmidt <isibboi@gmail.com>
2025-06-03 13:23:58 +0300

    Try make preconfigured scene.

--- local/home-assistant.nix
+++ local/home-assistant.nix
@@ -2,5 +2,16 @@
   services.home-assistant.config = {
     "automation ui" = "!include automations.yaml";
     "scene ui" = "!include scenes.yaml";
+
+    "scene manual" = [
+      {
+        name = "Daytime preconfigured";
+        entities = {
+          light.wiz_rgbw_tunable_f62fd1 = {
+            color_temp_kelvin = 3000;
+          };
+        };
+      }
+    ];
   };
 }
