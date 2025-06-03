8ab3e0b5b9d274244c75b65e57847af5ea3400f6
Sebastian Schmidt <isibboi@gmail.com>
2025-06-03 16:17:33 +0300

    Add more automations.

--- local/home-assistant.nix
+++ local/home-assistant.nix
@@ -5,6 +5,77 @@
 
     "automation manual" = [
       {
+        alias = "Bedroom morning on";
+        mode = "single";
+        triggers = [
+          {
+            trigger = "time";
+            at = "06:30:00";
+          }
+        ];
+        conditions = [
+          {
+            condition = "state";
+            entity_id = "light.bedroom";
+            state = "off";
+            for = { hours = 0; minutes = 1; seconds = 0; };
+          }
+        ];
+        actions = [
+          {
+            action = "scene.turn_on";
+            target.entity_id = "scene.wake_up";
+          }
+        ];
+      }
+
+      {
+        alias = "Bedroom morning off";
+        mode = "single";
+        triggers = [
+          {
+            trigger = "time";
+            at = "08:50:00";
+          }
+        ];
+        actions = [
+          {
+            action = "light.turn_off";
+            target.entity_id = "light.bedroom";
+          }
+        ];
+      }
+
+      {
+        alias = "Bedroom evening on";
+        mode = "single";
+        triggers = [
+          {
+            trigger = "time";
+            at = "17:30:00";
+          }
+        ];
+        conditions = [
+          {
+            condition = "state";
+            entity_id = "light.bedroom";
+            state = "off";
+            for = { hours = 0; minutes = 1; seconds = 0; };
+          }
+        ];
+        actions = [
+          {
+            action = "light.turn_on";
+            target.entity_id = "light.bedroom";
+            data = {
+              brightness = 255;
+              transition = 2;
+            };
+          }
+        ];
+      }
+
+      {
         alias = "Bedroom reminder dim";
         mode = "single";
         triggers = [
@@ -35,6 +106,42 @@
               transition = 2;
               brightness = 150;
             };
+          }
+        ];
+      }
+
+      {
+        alias = "Office daytime";
+        mode = "single";
+        triggers = [
+          {
+            trigger = "time";
+            at = "6:00:00";
+          }
+        ];
+        actions = [
+          {
+            action = "scene.turn_on";
+            target.entity_id = "scene.daytime";
+            data.transition = 120;
+          }
+        ];
+      }
+
+      {
+        alias = "Office evening dim";
+        mode = "single";
+        triggers = [
+          {
+            trigger = "time";
+            at = "19:00:00";
+          }
+        ];
+        actions = [
+          {
+            action = "scene.turn_on";
+            target.entity_id = "scene.relax";
+            data.transition = 10;
           }
         ];
       }
