a5dedb968e0f91e1f64ed11c79950c8f6befc728
Sebastian Schmidt <isibboi@gmail.com>
2025-06-03 15:49:04 +0300

    Update entities.

--- local/home-assistant.nix
+++ local/home-assistant.nix
@@ -46,17 +46,7 @@
         name = "Daytime";
         icon = "mdi:white-balance-sunny";
         entities = {
-          "light.wiz_rgbw_tunable_f62fd1" = {
-            state = "on";
-            color_temp_kelvin = 3000;
-            color_mode = "color_temp";
-          };
-          "light.wiz_rgbw_tunable_f63007" = {
-            state = "on";
-            color_temp_kelvin = 3000;
-            color_mode = "color_temp";
-          };
-          "light.wiz_rgbw_tunable_f63def" = {
+          "light.sebastian_room" = {
             state = "on";
             color_temp_kelvin = 3000;
             color_mode = "color_temp";
@@ -68,17 +58,7 @@
         name = "Relax";
         icon = "mdi:sofa-single";
         entities = {
-          "light.wiz_rgbw_tunable_f62fd1" = {
-            state = "on";
-            rgbw_color = [100 245 249 155];
-            color_mode = "rgbw";
-          };
-          "light.wiz_rgbw_tunable_f63007" = {
-            state = "on";
-            rgbw_color = [100 245 249 155];
-            color_mode = "rgbw";
-          };
-          "light.wiz_rgbw_tunable_f63def" = {
+          "light.sebastian_room" = {
             state = "on";
             rgbw_color = [100 245 249 155];
             color_mode = "rgbw";
