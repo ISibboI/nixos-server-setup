8f05043f1e9893b59b4d58014018ffe46a3919c8
Sebastian Schmidt <isibboi@gmail.com>
2025-07-28 13:01:52 +0300

    Update mailserver.

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -21,10 +21,10 @@ in {
       # Mailserver.
       (builtins.fetchTarball {
         # Pick a release version you are interested in and set its hash, e.g.
-        url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/b9e28e23af46f4b5c4ca287ea8ce46e5b39e723c/nixos-mailserver-b9e28e23af46f4b5c4ca287ea8ce46e5b39e723c.tar.gz";
+        url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/ce87c8a9771d1a20c3fa3b60113b9b0821627dcb/nixos-mailserver-ce87c8a9771d1a20c3fa3b60113b9b0821627dcb.tar.gz";
         # To get the sha256 of the nixos-mailserver tarball, we can use the nix-prefetch-url command:
         # nix-prefetch-url "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/master/nixos-mailserver-master.tar.gz" --unpack
-        sha256 = "14zhxa6ig7wf0pr1ya3hc4frbb8rpj5ac1pmpd28lh5cbqcnr2yf";
+        sha256 = "1ba1z39plhlnyd1hjg3pgxmwgjlzlxjg4j8fqv03a72yagdv9xm1";
       })
     ];
 
