045dd568156cbbf0dc3e590b3675d53cc48bb631
Sebastian Schmidt <isibboi@gmail.com>
2026-03-26 10:03:03 +0200

    Possibly fix numpy.

--- local/configuration.nix
+++ local/configuration.nix
@@ -242,6 +242,23 @@ in {
     };
   };
 
+  # Fix numpy CPU support for home assistant.
+  nixpkgs.overlays = [
+    (final: prev: {
+      python3 = prev.python3.override {
+        packageOverrides = pyFinal: pyPrev: {
+          numpy = pyPrev.numpy.overrideAttrs (old: {
+            # Optional: NumPy's Meson options (example)
+            mesonFlags = (old.mesonFlags or []) ++ [
+              "-Dcpu-baseline=native"
+              "-Dcpu-dispatch=none"
+            ];
+          });
+        };
+      };
+    })
+  ];
+
   # Mosquitto
   services.mosquitto = {
     enable = true;
