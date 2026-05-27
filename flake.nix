3c8f78782b811f30eea596294a099034ca685c2f
Sebastian Schmidt <isibboi@gmail.com>
2026-05-27 08:56:51 +0300

    Different nixpkgs commit for radd.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     radd.url = "github:ISibboI/radd";
-    radd.inputs.nixpkgs.follows = "nixpkgs";
+    radd.inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable/b71c3965ae02e6788567965c91976020509a0710";
   };
   outputs = { self, nixpkgs, ... }@attrs: {
     nixosConfigurations.hetzner = nixpkgs.lib.nixosSystem {
