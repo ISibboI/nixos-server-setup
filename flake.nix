fc0d7c3e744d47b8dd22b078c6f39b7686047e5b
Sebastian Schmidt <isibboi@gmail.com>
2026-05-27 09:03:43 +0300

    Add comment explaining pinning nixpkgs for radd.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     radd.url = "github:ISibboI/radd";
+    # Newer nixpkgs cmake does not work on x86-64 anymore...
     radd.inputs.nixpkgs.url = "github:NixOS/nixpkgs/b71c3965ae02e6788567965c91976020509a0710";
   };
   outputs = { self, nixpkgs, ... }@attrs: {
